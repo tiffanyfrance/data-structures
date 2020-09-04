e6a7116729f778de043e31043e3ba8149eb7f11d
EC2 Default User <ec2-user@ip-172-31-8-234.us-east-2.compute.internal>
2020-09-04 20:56:32 +0000

    assignment 1

---
+++ wa01/wa01.js
@@ -0,0 +1,29 @@
+// npm install request
+// mkdir data
+
+var request = require('request');
+var fs = require('fs');
+
+var urlArr = [
+    "https://parsons.nyc/aa/m01.html",  
+    "https://parsons.nyc/aa/m02.html",  
+    "https://parsons.nyc/aa/m03.html",  
+    "https://parsons.nyc/aa/m04.html",  
+    "https://parsons.nyc/aa/m05.html",  
+    "https://parsons.nyc/aa/m06.html",  
+    "https://parsons.nyc/aa/m07.html",  
+    "https://parsons.nyc/aa/m08.html",  
+    "https://parsons.nyc/aa/m09.html",  
+    "https://parsons.nyc/aa/m10.html"
+];
+
+// the for loop uses let instead of var in order to properly scope the i
+for(let i = 0; i < urlArr.length; i++) {
+    request(urlArr[i], function(error, response, body){
+        if (!error && response.statusCode == 200) {
+            var urlPath = '/home/ec2-user/environment/wa01/data/m' + i + '.txt';
+            fs.writeFileSync(urlPath, body);
+        }
+        else {console.log("Request failed!")}
+    });
+}
