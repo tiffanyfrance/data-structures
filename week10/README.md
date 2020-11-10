7cd1f2e4c8c81d4ea5a9faab99ea33f450d87514
tiffylou <tiffany6872@gmail.com>
2020-11-10 15:30:00 -0500

    update 5 and add 10

---
+++ week10/README.md
@@ -0,0 +1,46 @@
+## Week 10: Interface Designs
+
+# Project 1: AA Map
+
+I wanted to focus on the user and approach this one from a UX perspective. Therefore, I asked questions about how can the user find what they need as quickly as possible.
+
+Basic design
+<img src="map1.jpg" width="200px">
+
+By Neighborhood
+- extra click for users
+- but may be a helpful navigation tool
+<img src="map2.jpg" width="200px">
+
+I know we are supposed to use leaflet, but wonder if maybe we can instead use something else? Mapbox, kepler, deckgl...
+
+Deckgl - I don't know if this is possible in the layers, but it would be really neat to add a layer of color for the location of the building.
+<img src="deckgl.png" width="200px">
+
+Kepler - here is a mockup of how that might look in kepler:
+<img src="kepler.png" width="200px">
+
+
+# Project 2: Recovery Blog
+
+Intitally for this project I wanted to write a daily poem, however because of a personal health issue, I decided to turn this into a visualization recording my recovery.
+
+The fields include date, pain level, mood, and various medicines taken (through IV and orally) for the ~month that I was in recovery from emergency surgery.
+
+<img src="recover.jpg"  width="200px">
+
+- tooltips are the only interaction
+- data will be mapped into line graphs and area charts that are curved into a layered ring. Pill numbers will be color coded and grouped by date. The circle represents days from Oct 1 - Nov 7.
+- The first task is to figure out how to map the dates (which will need to be formmatted on output) into a circle. Once I know where the coordinates are (I am thinking I will need pythagorian therom to get the math right), I can layer the circles outward and place the line graph on an x axis.
+- As seen in the picture
+- Ideally I would like this to be a poster type visualization, but because its data structures my challenge will be able to make this nice looking on all device widths. My assumption about the user is that they will want to zoom and pan to see details.
+
+
+# Project 3: Weather Sensor
+
+I haven't recieved the magnetic field sensor yet so I am using the temperature/humidity gauge.
+
+<img src="weather.jpg" width="200px">
+
+Interaction will be collective as seen in the multiple charts of this fivethirtyeight visualization: <a href="https://projects.fivethirtyeight.com/trump-approval-ratings/">https://projects.fivethirtyeight.com/trump-approval-ratings/</a>. 
+
--- week5/wa05.js
+++ week5/wa05.js
@@ -2,24 +2,66 @@ const async = require('async');
 
 var blogEntries = [];
 
+// class BlogEntry {
+//   constructor(postNum, date, postTitle, postEntry) {
+//     this.post_num = {};
+//     this.post_num.N = postNum.toString();
+//     this.date = {}; 
+//     this.date.S = new Date(date).toDateString();
+//     this.post_title = {};
+//     this.post_title.S = postTitle;
+//     this.post_entry = {};
+//     this.post_entry.S = postEntry;
+//     this.month = {};
+//     this.month.N = new Date(date).getMonth().toString();
+//   }
+
 class BlogEntry {
-  constructor(postNum, date, postTitle, postEntry) {
+  constructor(postNum,date,pain,mood,oxycodone,advil,tylenol,colace,cyclobenzaprine,amoxicillian,morphine,toradol,piperacillin,xanax) {
     this.post_num = {};
     this.post_num.N = postNum.toString();
     this.date = {}; 
     this.date.S = new Date(date).toDateString();
-    this.post_title = {};
-    this.post_title.S = postTitle;
-    this.post_entry = {};
-    this.post_entry.S = postEntry;
+    // this.post_title = {};
+    // this.post_title.S = postTitle;
+    // this.post_entry = {};
+    // this.post_entry.S = postEntry;
+    this.pain = {};
+    this.pain.N = pain.toString();
+    this.mood = {};
+    this.mood.N = mood.toString();
+    this.oxycodone = {};
+    this.oxycodone.N = oxycodone.toString();
+    this.advil = {};
+    this.advil.N = advil.toString();
+    this.tylenol = {};
+    this.tylenol.N = tylenol.toString();
+    this.colace = {};
+    this.colace.N = colace.toString();
+    this.cyclobenzaprine = {};
+    this.cyclobenzaprine.N = cyclobenzaprine.toString();
+    this.amoxicillian = {};
+    this.amoxicillian.N = amoxicillian.toString();
+    this.morphine = {};
+    this.morphine.N = morphine.toString();
+    this.toradol = {};
+    this.toradol.N = toradol.toString();
+    this.piperacillin = {};
+    this.piperacillin.N = piperacillin.toString();
+    this.xanax = {};
+    this.xanax.N = xanax.toString();
     this.month = {};
     this.month.N = new Date(date).getMonth().toString();
   }
 }
 
-blogEntries.push(new BlogEntry(1, 'October 1, 2020', 'Be here now', `When I throw a pot<br>There is no magic<br>I am waiting, as one who believes in magic,<br>For the magic to be revealed<br>And a majestic pot to manifest forth<br>From the universe<br>But it does not come<br>No<br>Instead I must rely on listening<br>And learning<br>And feeling<br>And trial and error<br>I am impatient<br>The pot folds in on itself<br>Slip splashes onto my forearms<br>A wet revealer of my lazy intention<br>My punishment is starting over<br>My punishment is wedging again<br>My punishment is misspent time`));
-blogEntries.push(new BlogEntry(2, 'October 2, 2020', 'Unfinished', `I don’t know why so many things are and aren’t<br>Why some live and some die<br>I don’t know why<br>But I want to trust<br>In the story of the lady of Gudalupe<br>Because it’s a lovely story<br>In the revelation of the tarot<br>Because it’s mutes the constant thoughts<br>In the sun pressing through the thick crowded clouds<br>Creating long rays of light<br>All the way from outer space how could it not<br>Be something important<br>How could it not<br>Mean something<br>Because I don’t want to be in a world<br>Where it means nothing<br>Because I don’t want fall into man’s limited interpretation of science<br>As the only reality for humanity<br>And I don’t want to believe that humanity is<br>Only human`));
-blogEntries.push(new BlogEntry(3, 'October 3, 2020', 'Patched', `I am twisted inside<br>Like the roots of a live oak<br>Left too long unmaintained<br>Underground limbs, curl, squeeze around a 20th century<br>Sewage drain<br>The pipes - rusted through - time-eaten holes<br>Leaking waste into the soil beneath the surface<br>Where polluted water permeates<br>A poison throughout, we are in need of a mend<br>A patch of this section<br>Fused to sections of the old<br>Which in time will need patching of their own`));
+// blogEntries.push(new BlogEntry(1, 'October 1, 2020', 'Be here now', `When I throw a pot<br>There is no magic<br>I am waiting, as one who believes in magic,<br>For the magic to be revealed<br>And a majestic pot to manifest forth<br>From the universe<br>But it does not come<br>No<br>Instead I must rely on listening<br>And learning<br>And feeling<br>And trial and error<br>I am impatient<br>The pot folds in on itself<br>Slip splashes onto my forearms<br>A wet revealer of my lazy intention<br>My punishment is starting over<br>My punishment is wedging again<br>My punishment is misspent time`));
+// blogEntries.push(new BlogEntry(2, 'October 2, 2020', 'Unfinished', `I don’t know why so many things are and aren’t<br>Why some live and some die<br>I don’t know why<br>But I want to trust<br>In the story of the lady of Gudalupe<br>Because it’s a lovely story<br>In the revelation of the tarot<br>Because it’s mutes the constant thoughts<br>In the sun pressing through the thick crowded clouds<br>Creating long rays of light<br>All the way from outer space how could it not<br>Be something important<br>How could it not<br>Mean something<br>Because I don’t want to be in a world<br>Where it means nothing<br>Because I don’t want fall into man’s limited interpretation of science<br>As the only reality for humanity<br>And I don’t want to believe that humanity is<br>Only human`));
+// blogEntries.push(new BlogEntry(3, 'October 3, 2020', 'Patched', `I am twisted inside<br>Like the roots of a live oak<br>Left too long unmaintained<br>Underground limbs, curl, squeeze around a 20th century<br>Sewage drain<br>The pipes - rusted through - time-eaten holes<br>Leaking waste into the soil beneath the surface<br>Where polluted water permeates<br>A poison throughout, we are in need of a mend<br>A patch of this section<br>Fused to sections of the old<br>Which in time will need patching of their own`));
+
+blogEntries.push(new BlogEntry(1,'October 1, 2020',9,4,6, 0,0,0,0,0,0,0,0,0));
+blogEntries.push(new BlogEntry(2,'October 2, 2020',9,4,4, 0,0,0,0,0,0,0,0,0));
+blogEntries.push(new BlogEntry(3,'October 3, 2020',8,3,6, 0,0,0,0,0,0,0,0,0));
 
 var AWS = require('aws-sdk');
 AWS.config = new AWS.Config();
@@ -30,7 +72,8 @@ var dynamodb = new AWS.DynamoDB();
 async.eachSeries(blogEntries, function(value, next) {
   var params = {};
   params.Item = value; 
-  params.TableName = "processblog";
+  // params.TableName = "processblog";
+  params.TableName = "processblog-recovery";
   
   dynamodb.putItem(params, function (err, data) {
     if (err) console.log(err, err.stack); // an error occurred
