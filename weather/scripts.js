895dc1bda5f36aea209263b4fcbc9fae75d47c24
tiffylou <tiffany6872@gmail.com>
2020-12-14 00:43:39 -0500

    tooltips

--- weather/scripts.js
+++ weather/scripts.js
@@ -2,8 +2,6 @@
 	let allData = await fetch('sensor.json');
 	allData = await allData.json();
 
-	allData = allData.filter(d => d.sensortemp < 200);
-
 	for (let i = 1; i < allData.length; i++) {
 		let diff = Math.abs(allData[i].sensortemp - allData[i - 1].sensortemp) / allData[i - 1].sensortemp;
 
@@ -34,6 +32,8 @@
 			dataMap[date] = arr;
 		}
 
+		d.sensortime = new Date(d.sensortime);
+
 		arr.push(d);
 	}
 
@@ -48,34 +48,40 @@
 
 	console.log(dataByDate);
 
-	window.foo = dataByDate;
-
 
 	let margin = { top: 40, right: 20, bottom: 20, left: 20 },
 		width = 250 - margin.left - margin.right,
 		height = 200 - margin.top - margin.bottom;
 
-	const tooltip = d3.select('body').append('div')
-		.attr('class', 'svg-tooltip');
+	let mouseMoves = [];
+
+
+
+	let formatDate = d3.timeFormat("%b %d, %Y");
 
 	for (let dbd of dataByDate) {
 		let day = dbd.data;
 
-		let svg = d3.select("#viz").append("svg")
+		let realSvg = d3.select('#viz').append('svg')
 			.attr('width', width + margin.left + margin.right)
-			.attr('height', height + margin.top + margin.bottom)
-			.append('g')
+			.attr('height', height + margin.top + margin.bottom);
+
+
+		let svg = realSvg.append('g')
 			.attr('transform', `translate(0,${margin.top})`)
 
+		let newDate = new Date(dbd.date);
+	
 		svg.append('text')
-			.text(d => dbd.date)
+			.text(formatDate(newDate))
+			.attr('class', 'date')
 			.attr('x', 0)
-			.attr('y', -10)
+			.attr('y', -15)
 			.attr('width', 100)
 			.attr('height', 20)
 
 		let x = d3.scaleUtc()
-			.domain(d3.extent(day, d => new Date(d.sensortime)))
+			.domain(d3.extent(day, d => d.sensortime))
 			.range([margin.left, width - margin.right]);
 
 		let y = d3.scaleLinear()
@@ -87,51 +93,114 @@
 			.call(d3.axisBottom(x)
 				.ticks(5)
 				.tickSizeOuter(0)
-				.tickFormat(d3.timeFormat("%I %p")));
+				.tickFormat(d3.utcFormat("%I %p")));
 
 		let yAxis = g => g
-			.attr("transform", `translate(${margin.left},0)`)
+			.attr('transform', `translate(${margin.left},0)`)
 			.attr('class', 'y-axis')
 			.call(d3.axisLeft(y).tickValues([20, 30, 40, 50, 60, 70, 80, 90]));
 
+		let verticalLine = svg.append('line')
+			.attr('class', 'vertical-line')
+			.attr('x1', 0)
+			.attr('y1', 0)
+			.attr('x2', 0)
+			.attr('y2', height)
+			.style('stroke-dasharray', ('3, 3'))
+			.style('stroke-opacity', 0.9)
+			.style('stroke', '#333')
+			.style('display', 'none');
+
 		let line = d3.line()
 			.curve(d3.curveCardinal)
-			.x(d => x(new Date(d.sensortime)))
+			.x(d => x(d.sensortime))
 			.y(d => y(d.sensortemp));
 
-		svg.append("g")
+		svg.append('g')
 			.call(xAxis)
-			.attr("transform", `translate(0,${((height - margin.bottom) / 2)})`);
+			.attr('transform', `translate(0,${((height - margin.bottom) / 2)})`);
 
-		svg.append("g")
+		svg.append('g')
 			.call(yAxis);
 
-		svg.append("path")
+		svg.append('path')
 			.datum(day)
-			.attr("fill", "none")
-			.attr("stroke", "steelblue")
-			.attr("stroke-width", 2)
-			.attr("stroke-linejoin", "round")
-			.attr("stroke-linecap", "round")
-			.attr("d", line)
-			.on('mouseover', function (e, d) {
-				// tooltip.style('visibility', 'visible');
+			.attr('fill', 'none')
+			.attr('stroke', 'steelblue')
+			.attr('stroke-width', 2)
+			.attr('stroke-linejoin', 'round')
+			.attr('stroke-linecap', 'round')
+			.attr('d', line);
 
-				// // let bisectDate = d3.bisector(d => new Date(d.date)).right;
-				// // let mouseDate = x.invert(d3.pointer(e)[0]);
-				// // let closest = data[bisectDate(data, mouseDate)];
+		let tooltip = svg.append('g')
+			.attr('class', 'tooltip')
+			.style('display', 'none');
 
-				console.log(d)
-				// tooltip
-				// 	.style('top', (e.pageY - 10) + 'px')
-				// 	.style('left', (e.pageX + 10) + 'px')
-				// 	.html(d);
-				// 	// .html(`${state.provinceState}<br>${closest.date}<br>${closest.count}`);
+		tooltip.append('rect')
+			.attr('x', -27)
+			.attr('width', 54)
+			.attr('y', -32)
+			.attr('height', 24)
+			.attr('fill', 'white');
+
+		let topText = tooltip.append('text')
+			.attr('y', -28);
+
+		let bottomText = tooltip.append('text')
+			.attr('y', -12);
+
+		tooltip.append('circle')
+			.attr('r', 2.5)
+			.attr('stroke', '#333')
+
+		realSvg
+			.on('mousemove', e => {
+				if (e.offsetX < x.range()[0] || e.offsetX > x.range()[1]) {
+					d3.selectAll('.tooltip').style('display', 'none');
+					d3.selectAll('.vertical-line').style('display', 'none');
+				} else {
+					for (let mm of mouseMoves) {
+						mm(e);
+					}
+				}
 			})
-			.on('mouseout', function () {
-				tooltip.style('visibility', 'hidden')
+			.on('mouseout', () => {
+				d3.selectAll('.tooltip').style('display', 'none');
+				d3.selectAll('.vertical-line').style('display', 'none');
 			})
 
+		mouseMoves.push((e) => {
+			let nearest = bisect(day, x.invert(e.offsetX));
+
+			if (!nearest) {
+				return;
+			}
+
+			topText.text(d3.utcFormat('%I:%M %p')(nearest.sensortime));
+			bottomText.text(`${Math.round(nearest.sensortemp)}°F`);
+
+			let posX = x(nearest.sensortime);
+
+			tooltip.attr('transform', `translate(${posX},${y(nearest.sensortemp)})`)
+				.style('display', null);
+
+			verticalLine.attr('x1', posX)
+				.attr('x2', posX)
+				.style('display', null);
+		});
+	}
+
+	const bisectDate = d3.bisector(d => d.sensortime).left;
+
+	function bisect(data, date) {
+		const i = bisectDate(data, date, 1);
+		const a = data[i - 1], b = data[i];
+
+		if (!a || !b) {
+			return null;
+		}
+
+		return date - a.sensortime > b.sensortime - date ? b : a;
 	}
 
 })();
